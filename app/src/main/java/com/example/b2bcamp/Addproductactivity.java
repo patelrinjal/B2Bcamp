fd4e19f1315ecc2205bf59fd5ffeba5c736f28e1
patelrinjal <b2b.camp1@gmail.com>
2020-01-03 12:20:44 +0530

    layout and addproduct activity (rinjal)

---
+++ app/src/main/java/com/example/b2bcamp/Addproductactivity.java
@@ -0,0 +1,14 @@
+package com.example.b2bcamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Addproductactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addproductactivity);
+    }
+}
