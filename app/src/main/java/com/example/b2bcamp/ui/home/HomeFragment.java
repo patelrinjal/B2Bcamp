fe860f880c5937ca9790f5297f2c75c1406d1f65
patelrinjal <b2b.camp1@gmail.com>
2020-02-21 11:58:51 +0530

    category product  (rinjal)

--- app/src/main/java/com/example/b2bcamp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/b2bcamp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.b2bcamp.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +9,15 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.b2bcamp.Categoryproductlistactivity;
+import com.example.b2bcamp.ForgotpasswordActivity;
 import com.example.b2bcamp.R;
+import com.example.b2bcamp.Sellerlogin;
 
 public class HomeFragment extends Fragment {
 
@@ -23,13 +28,18 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        final CardView cardView = root.findViewById(R.id.cb_category);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
+                Intent i=new Intent(getActivity(), Categoryproductlistactivity.class);
+                startActivity(i);
+
             }
-        });*/
+        });
+
         return root;
     }
 }
