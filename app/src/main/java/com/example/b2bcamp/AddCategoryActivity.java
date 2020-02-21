f789d83937a458c2dbd58eb5e8a1513dd1c93e26
VISHWA PATEL <b2b.camp1@gmail.com>
2020-02-21 12:26:12 +0530

    addcategoryproduct(VISHWA)

---
+++ app/src/main/java/com/example/b2bcamp/AddCategoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.b2bcamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category);
+    }
+}
