ad247f9178e4f10e8d33a2113f7b2a34a316f4e0
patelrinjal <b2b.camp1@gmail.com>
2020-02-25 11:40:33 +0530

    activity inquiry xml   (rinjal)

---
+++ app/src/main/java/com/example/b2bcamp/AddInquiryActivity.java
@@ -0,0 +1,15 @@
+package com.example.b2bcamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class AddInquiryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_inquiry);
+    }
+}
